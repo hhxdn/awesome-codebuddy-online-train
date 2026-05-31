6fb4fc1719fd2b6cdea040b0f921e9d1cb6904cb
ascendking <wangwenzhe163@163.com>
2026-05-31 11:20:41 +0800

    chore: 更新 components.d.ts 自动生成文件

--- h5/src/components.d.ts
+++ h5/src/components.d.ts
@@ -36,6 +36,8 @@ declare module 'vue' {
     VanRadio: typeof import('vant/es')['Radio']
     VanRadioGroup: typeof import('vant/es')['RadioGroup']
     VanSearch: typeof import('vant/es')['Search']
+    VanSwipe: typeof import('vant/es')['Swipe']
+    VanSwipeItem: typeof import('vant/es')['SwipeItem']
     VanTab: typeof import('vant/es')['Tab']
     VanTabbar: typeof import('vant/es')['Tabbar']
     VanTabbarItem: typeof import('vant/es')['TabbarItem']
